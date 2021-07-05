f91e19798f99aef9bb1a924b8641c6e5092f6f79
Евгений <47692960+DsJohn@users.noreply.github.com>
2021-07-06 00:50:00 +0500

    Update calc.php

--- calc.php
+++ calc.php
@@ -1,32 +1,21 @@
 <?
-/* var_dump($_POST); */
-
-
-   /* $DepositDate = DateTime::createFromFormat('Y-m-d', $_POST[DepositDate]);  */ 
    $DepositDate = date($_POST[DepositDate]);   
    $DepositAmount = intval($_POST[DepositAmount]);
    $DepositTerm = intval($_POST[DepositTerm]);
    $AddingToDepositAmount = intval($_POST[AddingToDepositAmount]); 
-
-   
-  
-   $daysn = cal_days_in_month(CAL_GREGORIAN, date('m',$_POST[DepositDate]), date('Y',$_POST[DepositDate])); //количество дней в месяце   
-   $daysy = date('L', mktime(1,1,1,1,1,$_POST[DepositDate]))?366:365; // количество дней в году, проверка на високосный год   
+   $daysn = cal_days_in_month(CAL_GREGORIAN, date('m',$_POST[DepositDate]), date('Y',$_POST[DepositDate]));  
+   $daysy = date('L', mktime(1,1,1,1,1,$_POST[DepositDate]))?366:365 
    $summn = 0; 
-   $summpre = $DepositAmount; //сумма на счете на конец прошлого месяца (или дня вклада депозита)
+   $summpre = $DepositAmount; 
    if ($_POST[AddingToDeposit] == "Yes") {
-   $summadd = $AddingToDepositAmount; // сумма ежемесячного пополнения депозита
+   $summadd = $AddingToDepositAmount;
    } else {
 	 $summadd = 0;  
    };
    $percent = 0.1;
-   
    $WorkingYear = date('Y',strtotime($DepositDate));
    $WorkingMonth = date('m',strtotime($DepositDate));
-   
    $WorkingMonth = date("$WorkingYear-$WorkingMonth-01");
-
-   
    for ($i = 1; $i <= $DepositTerm; $i++) {  
 	   for ($j = 1; $j <= 12; $j++) {
 		  if ($i==1 && $j==1) {
@@ -37,20 +26,12 @@
 		   } else { 		   
 			 $daysn = cal_days_in_month(CAL_GREGORIAN, date('m',strtotime($WorkingMonth)), date('Y',strtotime($WorkingMonth))); 
 		 		   };
-		   
 		  $summn = $summpre + ($summpre + $summadd)*$daysn*($percent / $daysy);
-		  $summpre = $summn;
-/* 		  echo json_encode($WorkingMonth); */		  
+		  $summpre = $summn;	  
 		  $WorkingMonth = date('Y-m-d', strtotime("+1 month", strtotime($WorkingMonth)));	
-		  
 	   };	    
 	   $daysy = date('L', mktime(1,1,1,1,1,($_POST[DepositDate])+1))?366:365;   
    }; 
- 
- 
-
-
-   /* echo json_encode($daysy); */
    $summn = ceil($summn - $DepositAmount);
    echo json_encode($summn);
 ?>
